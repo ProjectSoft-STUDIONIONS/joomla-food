9ef3df4573b9efdbc53aca1c20d6994102880113
ProjectSoft <projectsoft2009@yandex.ru>
2025-03-11 03:09:01 +0400

    update

---
+++ com_food/admin/helpers/food.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_food
+ *
+ * @copyright   Copyright (C) 2008 - All rights reserved.
+ * @license     MIT
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Food component helper.
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_food
+ * @since       1.6
+ */
+class LendrHelpersLendr
+{
+	public static $extension = 'com_food';
+
+	/**
+	 * @return  JObject
+	 */
+	public static function getActions()
+	{
+		$user	= JFactory::getUser();
+		$result	= new JObject;
+
+		$assetName = 'com_food';
+		$level = 'component';
+
+		$actions = JAccess::getActions('com_food', $level);
+
+		foreach ($actions as $action)
+		{
+			$result->set($action->name,	$user->authorise($action->name, $assetName));
+		}
+
+		return $result;
+	}
+}
---
+++ com_food/admin/models/statistics.php
@@ -0,0 +1,14 @@
+<?php // no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
+ 
+class FoodModelsStatistics extends JModelBase
+{
+	
+	public function getStats()
+	{
+		$stats = array();
+		return $stats;
+	}
+
+}
